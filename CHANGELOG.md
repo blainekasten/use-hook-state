d9c98587f43ca75311f810d704a4f5540def4776
Blaine Kasten <blainekasten@gmail.com>
2020-12-21 11:23:23 -0600

    0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-# 1.0.0
+# 0.1.1
 
-First release!
+- Add Types
+- Add utility to change the default value
+
+# 0.1.0
+
+- Test Release!
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,48 @@ import { useEffect, useState } from 'react'
 type Hook<State> = () => State
 type Responders<State> = Array<(newState: State) => void>
 
-export function createState<State>(defaultValue: State, useHook: Hook<State>) {
+type HookState<State> = {
+  useState(): State
+  Provider: {
+    (): null
+    setDefaultValue(newDefaultValue: State): void
+  }
+}
+
+export function createState<State>(
+  defaultValue: State,
+  useHook: Hook<State>
+): HookState<State> {
   let initialValue = defaultValue
   let unmounted = false
   let responders: Responders<State> = []
 
+  function Provider(): null {
+    unmounted = false
+    let value = useHook()
+    initialValue = value
+
+    useEffect(() => {
+      return () => {
+        unmounted = true
+      }
+    }, [])
+
+    responders.forEach((r) => r(value))
+
+    return null
+  }
+
+  // This is a testing utility. Should probably lock it down to be only in test.
+  // @ts-ignore
+  Provider.setDefaultValue((newDefaultValue: State): void => {
+    initialValue = newDefaultValue
+  })
+
+  // Public API
   return {
+    // @ts-ignore
+    Provider,
     useState(): State {
       if (unmounted) {
         throw new Error('Cannot get state from unmounted Provider')
@@ -29,21 +65,6 @@ export function createState<State>(defaultValue: State, useHook: Hook<State>) {
       }, [])
 
       return state
-    },
-    Provider(): null {
-      unmounted = false
-      let value = useHook()
-      initialValue = value
-
-      useEffect(() => {
-        return () => {
-          unmounted = true
-        }
-      }, [])
-
-      responders.forEach((r) => r(value))
-
-      return null
     },
   }
 }
